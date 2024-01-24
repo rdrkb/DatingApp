954bdd9c5f51334d54f836d9a4539cdd0a4e6c94
rkbrd <riad.net.101@gmail.com>
2024-01-25 01:34:08 +0600

    end of 37

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,10 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-
-public class UsersController : ControllerBase
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
 
